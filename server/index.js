f32649da86fa3108ecd760a42d58f2516058f268
gbudjeakp <gbudjeakp@gmail.com>
2022-01-27 23:43:28 -0600

    Updating server side code

---
+++ server/index.js
@@ -0,0 +1,14 @@
+//Create express app
+const express = require('express'); //import express
+const app = express(); //create express app
+
+
+//get end point
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+});
+
+//Listen on port 3000
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
